b4bf13c8da9df047faf8335ca3cef65845c66b3c
Pavel Nikolaev <pavel.nikolaev@tele2.ru>
2023-08-27 02:03:02 +0300

    Add git updates

--- src/runner/index.js
+++ src/runner/index.js
@@ -1,5 +1,6 @@
 import logsManager from '../logsManager'
 import { spawn } from 'child_process'
+import { runCommand } from '../tools/tools'
 
 let index = 0
 
@@ -11,8 +12,48 @@ const createRunner = (params) => {
     let isStarted = false
 
     const run = () => {
-        return new Promise((resolve) => {
-            logsManager.append(`processs ${_index} spawned.`)
+        return new Promise(async (resolve) => {
+            logsManager.append(`process ${_index} spawned.`)
+
+            const onLog = (log) => {
+                logsManager.append(`process ${_index} (git): ${log}`)
+            }
+
+            await runCommand({
+                command: 'git reset',
+                cwd: params.path,
+                onLog
+            })
+
+            await runCommand({
+                command: 'git fetch',
+                cwd: params.path,
+                onLog
+            })
+
+            await runCommand({
+                command: 'git checkout .',
+                cwd: params.path,
+                onLog
+            })
+
+            await runCommand({
+                command: 'git clean -fdx',
+                cwd: params.path,
+                onLog
+            })
+
+            await runCommand({
+                command: `git checkout master`,
+                cwd: params.path,
+                onLog
+            })
+
+            await runCommand({
+                command: `git pull`,
+                cwd: params.path,
+                onLog
+            })
 
             let command = `yarn start -port:${params.port}`
             process = spawn(command.split(' ')[0], command.split(' ').slice(1, command.split(' ').length), {
@@ -26,21 +67,21 @@ const createRunner = (params) => {
                     logsManager.append(`Runner ${_index} started. Asked port - ${params.port}`)
                     resolve()
                 }
-                logsManager.append(`processs ${_index} (out): ${`${data}`.trim()}`)
+                logsManager.append(`process ${_index} (out): ${`${data}`.trim()}`)
             })
 
             process.stderr.on('data', (data) => {
-                logsManager.append(`processs ${_index} (err): ${`${data}`.trim()}`)
+                logsManager.append(`process ${_index} (err): ${`${data}`.trim()}`)
             })
 
             process.on('close', (code) => {
-                logsManager.append(`processs ${_index} (close): ${code}`)
+                logsManager.append(`process ${_index} (close): ${code}`)
             })
         })
     }
 
     const stop = () => {
-        logsManager.append(`processs ${_index} killed.`)
+        logsManager.append(`process ${_index} killed.`)
         process.kill(0)
     }
 
@@ -52,3 +93,15 @@ const createRunner = (params) => {
 }
 
 export { createRunner }
+
+/*
+    { value: , cwd: '|||PROJECT_DIR|||' },
+        { value: , cwd: '|||PROJECT_DIR|||' },
+        { value: , cwd: '|||PROJECT_DIR|||' },
+        { value: , cwd: '|||PROJECT_DIR|||' },
+        {
+            value: `git checkout ${buildParams.branchName ? buildParams.branchName : 'develop'}`,
+            cwd: '|||PROJECT_DIR|||'
+        },
+        { value: `git pull`, cwd: '|||PROJECT_DIR|||' },
+*/
--- src/tools/tools.js
+++ src/tools/tools.js
@@ -1,4 +1,5 @@
 import fs from 'fs/promises'
+import { spawn } from 'child_process'
 
 const fileExists = async (path) => {
     try {
@@ -9,4 +10,28 @@ const fileExists = async (path) => {
     }
 }
 
-export { fileExists }
+const runCommand = ({ command, cwd, onLog }) => {
+    return new Promise((resolve) => {
+        process = spawn(command.split(' ')[0], command.split(' ').slice(1, command.split(' ').length), {
+            cwd
+        })
+
+        process.stdout.on('data', (data) => {
+            if (onLog) {
+                onLog(`${data}`.trim())
+            }
+        })
+
+        process.stderr.on('data', (data) => {
+            if (onLog) {
+                onLog(`${data}`.trim())
+            }
+        })
+
+        process.on('close', (code) => {
+            resolve(code)
+        })
+    })
+}
+
+export { fileExists, runCommand }
